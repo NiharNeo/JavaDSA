b236549aa5893e631d728f6978b536812efbd132
NiharNeo <niharchavan648@gmail.com>
2026-08-19 10:54:40 +0530

    Constructor Chaning in same Class

---
+++ src/DAY3/SESSION1/ConstructorChaning.java
@@ -0,0 +1,27 @@
+package DAY3.SESSION1;
+
+// Same Class
+
+class Std{
+    String name;
+    String course;
+    int id;
+
+    public Std(String name , String course , int id){
+        this.course = course;
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public Std(){
+        this("Nihar","DAS",63);
+    }
+}
+
+public class ConstructorChaning {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/DAY3/SESSION1/notes.md
@@ -0,0 +1,8 @@
+# Construction Chaning
+    1. In same class
+    2. In a differnt class
+
+    1. For same class :
+        use .this(); 
+    2. For different class:
+        use .super();
