d6c4dad6438dae30033e85642de5a1e6f7bad507
NiharNeo <niharchavan648@gmail.com>
2026-09-01 10:41:19 +0530

    NextGreater Element 2

---
+++ src/DAY10/SESSION1/NextGreaterElement2.java
@@ -0,0 +1,29 @@
+package DAY10.SESSION1;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public int[] nextGreaterElements(int[] nums) {
+    int n = nums.length;
+    int[] result = new int[n];
+    Arrays.fill(result, -1);
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = 0; i < 2 * n; i++) {
+        int cur = nums[i % n];
+
+        while (!stack.isEmpty() && nums[stack.peek()] < cur) {
+            int index = stack.pop();
+            result[index] = cur;
+        }
+
+        if (i < n) {
+            stack.push(i % n);
+        }
+    }
+    return result;
+}
+    public static void main(String[] args){
+
+    }
+}
