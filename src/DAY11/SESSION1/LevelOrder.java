1f5dfdf98274f07b5fdf6ccef4c850d250e55fab
NiharNeo <niharchavan648@gmail.com>
2026-09-02 11:43:01 +0530

    Level Order Tree TraverslJava

---
+++ src/DAY11/SESSION1/LevelOrder.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Nodee {
+    int data;
+    Nodee left, right;
+
+    Nodee(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class LevelOrder {
+    public static List<List<Integer>> levelOrder(Nodee root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Nodee> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                Nodee node = queue.poll();
+                level.add(node.data);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
